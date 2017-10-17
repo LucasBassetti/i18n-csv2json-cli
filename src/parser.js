bad42ccd95884c2eced747c54cff7541b2b74da4
Tim <truecarry@gmail.com>
2017-10-17 03:56:44 +0300

    Move to different csv parse function

--- src/parser.js
+++ src/parser.js
@@ -12,13 +12,30 @@ function stringToObj(path, value, obj) {
 
   obj[last] = value;
 }
+//Function that makes array out of csv file
+function csvToArray (text) {
+  let p = '', row = [''], ret = [row], i = 0, r = 0, s = !0, l;
+  for (l in text) {
+    l = text[l];
+    if ('"' === l) {
+      if (s && l === p) row[i] += l;
+      s = !s;
+    } else if (',' === l && s) l = row[++i] = '';
+    else if ('\n' === l && s) {
+      if ('\r' === p) row[i] = row[i].slice(0, -1);
+      row = ret[++r] = [l = '']; i = 0;
+    } else row[i] += l;
+    p = l;
+  }
+  return ret;
+}
 
 function parseFile({ options, data }) {
   return new Promise((resolve) => {
     const { from } = options;
     const splitEl = /.csv$/.test(from) ? ',' : '\t';
-    const lines = data.split('\n').map(line => line.replace(/(\r|'|")/g, ''));
-    const files = lines[0].split(splitEl);
+    const lines = csvToArray(data)
+    const files = lines[0]
     const result = {};
 
     lines.splice(0, 1);
@@ -29,7 +46,7 @@ function parseFile({ options, data }) {
     }
 
     for (let i = 0, len = lines.length; i < len; i += 1) {
-      const columns = lines[i].split(splitEl);
+      const columns = lines[i]
 
       for (let j = 1, jlen = columns.length; j < jlen; j += 1) {
         const key = columns[0];
